0280ca122c0697564d3e263ccb96c2f209874aca
Leoana <leoana1705@gmail.com>
2020-07-19 10:48:37 -0500

    indicando algo basico de ttesting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+// Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SummaryComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
