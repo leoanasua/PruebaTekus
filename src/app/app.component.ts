68fa4b45392f0c1ac9eeac1f9b467e84237f217a
Leoana <leoana1705@gmail.com>
2020-07-21 11:08:12 -0500

    cambios para obtener un selector de idioma en la tabla de cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,4 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class AppComponent {
   title = 'Tekus';
-  langs: string [] = [];
-
-  constructor(private translate: TranslateService){
-    this.translate.setDefaultLang('en'); // Selecting default language
-    this.translate.use('es'); // language in which page will be translated
-    this.translate.get('Name') // cheking translations from component by console
-    .subscribe ((res: string) => {
-    console.log(res);
-    });
-  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataserviceService } from '../../services/dataservice.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +10,27 @@ import { DataserviceService } from '../../services/dataservice.service';
 export class HomeComponent implements OnInit {
 
   cities: any = [];
+  langs: string [] = [];
 
-  constructor(private dataService: DataserviceService) { }
+  constructor(private dataService: DataserviceService, private translate: TranslateService) {
+  this.translate.setDefaultLang('en');
+  this.translate.use('en');
+  this.translate.addLangs(['en', 'es', 'fr']);
+  this.langs = this.translate.getLangs();
+  }
+
+  changeLang(lang: string) {
+  this.translate.use(lang);
+  }
 
   ngOnInit(): void {
-    this.getCities();
+  this.getCities();
   }
 
   getCities() {
     this.dataService.getCities()
     .subscribe( (data: any) => {
-      this.cities = data;
+    this.cities = data;
     });
   }
 }
